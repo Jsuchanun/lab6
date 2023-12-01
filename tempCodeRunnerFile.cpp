b58a2ebd86c1afc02a24c3d04fd6cd6c58e7f7f8
suchanun <suchaun_j@cmu.ac.th>
2023-12-01 16:00:51 +0700

    lab6_1.cpp completed

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout << "Enter an integer: ";
+    cin >> x;
+    int even = 0, odd = 0;
+    while (x != 0){
+        if(x%2 == 0){
+            even = even+1;
+        }else{
+            odd = odd+1;
+        }
+    cout << "Enter an integer: ";
+    cin >> x;  
+
+    }
+    cout << "#Even numbers = " << even << "\n";
+    cout << "#Odd numbers = " << odd;
+    return 0;
+}
